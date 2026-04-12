723830095396c1329e880f09d6fc739eaf330276
KUROYUKIHIME333 <dhermansum@gmail.com>
2026-04-12 22:40:42 +0100

    stc parameters for a pannel

--- src/types/installationPhotovoltaique.types.ts
+++ src/types/installationPhotovoltaique.types.ts
@@ -31,4 +31,20 @@ export type PompageSolaireCaracteristiques = {
     // Q = débit en m³/j
     // H_man = hauteur manométrique totale en m
     // η_pompe = rendement pompe (0,4–0,7)
+};
+
+export type ParametresSTCPanneau = {
+    puissanceCreteModule: number, // (Wp) Puissance en point MPP en STC
+    tensionCircuitOuvert: number, // Voc; Tension à courant nul — détermine tenue des équipements
+    courantCourtCircuit: number, // Isc; Courant max — dimensionne câbles et protections
+    tensionMPP: number, // Vmpp; Tension au point de puissance max
+    courantMPP: number, // Impp; Courant au point de puissance max
+    coeffTempTension: number, // β (Voc); Typique : −0,30 à −0,45 %/°C
+    coeffTempPuissance: number, // γ (Pmax); Typique : −0,35 à −0,45 %/°C
+};
+
+export type TemperaturesMinMax = {
+    temperatureMin: number,
+    temperatureMax: number
 }
+
