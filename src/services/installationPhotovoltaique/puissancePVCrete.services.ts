49dfa39f857101a5da59908cb83061273591ed2c
KUROYUKIHIME333 <dhermansum@gmail.com>
2026-04-12 22:40:56 +0100

    pvModules

--- src/services/installationPhotovoltaique/puissancePVCrete.services.ts
+++ src/services/installationPhotovoltaique/puissancePVCrete.services.ts
@@ -1,4 +1,4 @@
-import type { TypeInstallationPourPertes, Localisation, PompageSolaireCaracteristiques } from "../../types/installationPhotovoltaique.types.js"
+import type { TypeInstallationPourPertes, Localisation, PompageSolaireCaracteristiques, ParametresSTCPanneau, TemperaturesMinMax } from "../../types/installationPhotovoltaique.types.js"
 
 export class PuissanceCretePVService {
     async performanceRatio(typeInstallation: TypeInstallationPourPertes | string, localisation: Localisation) {
@@ -66,7 +66,60 @@ export class PuissanceCretePVService {
         return P_PV;
     };
 
-    modulesPV(){};
+    modulesPV(panneauParametres: ParametresSTCPanneau, puissanceCretePV: number, temperaturesAttendue: TemperaturesMinMax, dureeEnsoleillement: number) {
+        const { puissanceCreteModule, tensionMPP, coeffTempTension, coeffTempPuissance } = panneauParametres;
+        const { temperatureMin, temperatureMax } = temperaturesAttendue;
+        let tension_system: number = 12;
+
+        // Tension du systeme
+        if (puissanceCretePV >= 500 && puissanceCretePV < 2000) {
+            tension_system = 24;
+        };
+        if (puissanceCretePV >= 2000 && puissanceCretePV < 10000) {
+            tension_system = 48;
+        };
+        if (puissanceCretePV >= 10000) {
+            tension_system = 96;
+        };
+
+        // Correction de tension selon température (T en °C par rapport à STC)
+        const tension_mpp_min = tensionMPP * (1 + coeffTempTension * (temperatureMax - 25));
+        const tension_mpp_max = tensionMPP * (1 + coeffTempTension * (temperatureMin - 25));
+
+        // Correction de la puissance selon température (T en °C par rapport à STC)
+        const puissance_crete_module_min = puissanceCreteModule * (1 + coeffTempPuissance * (temperatureMax - 25));
+        const puissance_crete_module_max = puissanceCreteModule * (1 + coeffTempPuissance * (temperatureMin - 25));
+
+        //Nombre de strings (modules montés en serie)
+        // Modules en série (par string) : la tension DC doit être compatible avec la plage MPPT de l'onduleur.
+        const nombre_strings_max = Math.floor(tension_system / tension_mpp_max);
+        const nombre_strings_min = Math.ceil(tension_system / tension_mpp_min);
+        const nombre_strings = Math.round((nombre_strings_max + nombre_strings_min) / 2);
+
+        // Strings en parallele
+        const nombre_strings_parallele = Math.ceil(Math.ceil(puissanceCretePV / puissance_crete_module_min) / nombre_strings);
+
+        // Nombre total de panneaux
+        const nombre_total_panneaux = nombre_strings_parallele * nombre_strings;
+
+        // Puissance PV installée
+        const puissancePVInstalleMin = nombre_total_panneaux * puissance_crete_module_min;
+        const puissancePVInstalleMax = nombre_total_panneaux * puissance_crete_module_max;
+
+        // ⚠ NOTE : Vérifier que Voc(T_min) × N_série ≤ tension max admissible de l'onduleur (typiquement
+        // 600 V, 1000 V ou 1500 V selon gamme)
+
+        return {
+            tensionPV: tension_system, // en V DC
+            panneauxParString: nombre_strings,
+            stringsEnParallele: nombre_strings_parallele,
+            totalPanneaux: nombre_total_panneaux,
+            puissancePVInstallee: {
+                min: puissancePVInstalleMin,
+                max: puissancePVInstalleMax
+            }
+        }
+    };
 };
 
 export const puissanceCretePVService = new PuissanceCretePVService();
