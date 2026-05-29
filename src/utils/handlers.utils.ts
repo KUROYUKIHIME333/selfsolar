f628acb4751e0c5219a658a718c379d283c07522
KUROYUKIHIME333 <dhermansum@gmail.com>
2026-05-29 11:37:46 +0100

    success response handler

--- src/utils/handlers.utils.ts
+++ src/utils/handlers.utils.ts
@@ -12,25 +12,12 @@ export const controllerErrorHandler = (error: unknown, name: string) => {
   };
 };
 
+
 // Fonctions utilitaires pour centraliser les réponses HTTP
 export const sendSuccess = (reply: FastifyReply, data: any, code = 200) => {
   return reply.code(code).send({
     success: true,
     error: null,
     data: data,
-  });
-};
-
-export const sendError = (reply: FastifyReply, error: any, code = 400) => {
-  // Si l'erreur provient de controllerErrorHandler et possède déjà une structure imbriquée
-  const errorMessage =
-    error && typeof error === "object"
-      ? error.message || error.error || JSON.stringify(error)
-      : error;
-
-  return reply.code(code).send({
-    success: false,
-    error: errorMessage,
-    data: null,
   });
 };
