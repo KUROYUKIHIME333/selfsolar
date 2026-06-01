722d9bdbb2751458d805550308c53c97c3cf40e1
KUROYUKIHIME333 <dhermansum@gmail.com>
2026-06-01 09:11:39 +0100

    Centralizing success error data format of responses

--- src/utils/handlers.utils.ts
+++ src/utils/handlers.utils.ts
@@ -12,27 +12,31 @@ export const controllerErrorHandler = (error: unknown, name: string) => {
   };
 };
 
-
-// Fonctions utilitaires pour centraliser les réponses HTTP
-export const sendSuccess = (reply: FastifyReply, data: any, code = 200) => {
-  return reply.code(code).send({
-    success: true,
-    error: null,
-    data: data,
-  });
+// Format standard des reponses
+export const sendResponse = (
+  successValue: boolean,
+  errorValue: string | null | undefined,
+  dataValue: null | undefined | string | number | Record<string, unknown>
+) => {
+  return {
+    success: successValue,
+    error: errorValue,
+    data: dataValue,
+  };
 };
 
-
+// Fonctions utilitaires pour centraliser les réponses HTTP (controlleurs)
+export const sendSuccess = (reply: FastifyReply, data: any, code = 200) => {
+  const response = sendResponse(true, null, data);
+  return reply.code(code).send(response);
+};
 export const sendError = (reply: FastifyReply, error: any, code = 400) => {
   // Si l'erreur provient de controllerErrorHandler et possède déjà une structure imbriquée
   const errorMessage =
     error && typeof error === "object"
       ? error.message || error.error || JSON.stringify(error)
       : error;
+  const response = sendResponse(false, errorMessage, null);
 
-  return reply.code(code).send({
-    success: false,
-    error: errorMessage,
-    data: null,
-  });
+  return reply.code(code).send(response);
 };
