802594dbd5f953b3ee0201db1a2206b6955316d5
KUROYUKIHIME333 <dhermansum@gmail.com>
2026-06-12 13:57:48 +0100

    update

--- src/routes/api.routes.ts
+++ src/routes/api.routes.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance } from "fastify";
-import { installationPhotovoltaiqueRoutes } from "./installationPhotovoltaique.routes.js";
+import { installationPhotovoltaiqueRoutes } from "./routesFiles/installationPhotovoltaique.routes.js";
 
 export const apiRoutes = async (app: FastifyInstance) => {
   // Routes PV sous préfixe /api/v1/pv
