9b5540b360cc97240985543bd729aafb88430cfd
KUROYUKIHIME333 <dhermansum@gmail.com>
2026-06-13 15:38:25 +0100

    update

--- src/utils/toolbox.utils.ts
+++ src/utils/toolbox.utils.ts
@@ -1,19 +1,15 @@
 export const getKeysInAnArray = (obj: Record<string, any>): string[] => {
-  // Use Object.keys to create an array of property names
   return Object.keys(obj);
 };
 
 export const getValuesInAnArray = (obj: Record<string, any>): string[] => {
-  // Use Object.keys to create an array of property names
   return Object.values(obj);
 };
 
 export const getKeysCommaSeparated = (obj: Record<string, any>): string => {
-  // Use Object.keys to create an array of property names
   return Object.keys(obj).join(",");
 };
 
 export const getValuesCommaSeparated = (obj: Record<string, any>): string => {
-  // Use Object.values to create an array of property values
   return Object.values(obj).join(",");
 };
