91d98014ec825b4a3b7ae651118c678f5eb48f52
KUROYUKIHIME333 <dhermansum@gmail.com>
2026-06-13 14:50:32 +0100

    request to create a subscription plan

--- src/db/requests/subscriptionPlanAndSubscription/requests.ts
+++ src/db/requests/subscriptionPlanAndSubscription/requests.ts
@@ -5,12 +5,30 @@ import type {
   DbSubscriptions,
 } from "../../../types/dbTypes.js";
 import db from "../../../config/db.js";
+import {
+  getKeysCommaSeparated,
+  getValuesCommaSeparated,
+} from "../../../utils/toolbox.utils.js";
+import { sendResponse } from "../../../utils/handlers.utils.js";
 
 export class SubscriptionsRequests {
   public async createSubscriptionPlan(newPlan: DbSubscriptionPlans) {
+    const columns = getKeysCommaSeparated(newPlan);
+    const values = getValuesCommaSeparated(newPlan);
 
-    const subscriptionPlan = await db`insert into ${SUBSCRIPTIONS_PLAN} ${db(
-      newPlan
-    )}`;
+    const subscriptionPlan =
+      await db`INSERT INTO ${SUBSCRIPTIONS_PLAN}(${columns}) VALUES(${values}) ON CONFLICT(id, plan) DO NOTHING RETURNING *`;
+
+    if (!subscriptionPlan) {
+      return sendResponse(
+        false,
+        "Le plan de souscription n'a pas été créé",
+        null
+      );
+    }
+
+    return sendResponse(true, null, subscriptionPlan);
   }
 }
+
+export const subscriptionsRequests = new SubscriptionsRequests();
