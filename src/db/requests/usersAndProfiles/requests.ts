ed6bf3eabb18178ecc357185c27bf2a3f1cb3c48
KUROYUKIHIME333 <dhermansum@gmail.com>
2026-06-13 19:35:03 +0100

    working

--- src/db/requests/usersAndProfiles/requests.ts
+++ src/db/requests/usersAndProfiles/requests.ts
@@ -16,11 +16,15 @@ export class ProfilesRequests {
         null
       );
     }
+
     const columns = getKeysCommaSeparated(newUser);
     const values = getValuesCommaSeparated(newUser);
 
     const profile =
-      await db`INSERT INTO ${PROFILES}(${columns}) VALUES(${values}) ON CONFLICT(email) DO NOTHING RETURNING *`;
+      await db`INSERT INTO ${PROFILES}(${columns}) VALUES(${values}) ON CONFLICT(email) DO NOTHING`;
+
+    //TODO: Remove it when finishing working or debugging
+    console.log("CREATION D'UN PROFILE :", profile);
 
     if (!profile) {
       return sendResponse(false, "L'email fourni est dejà utilisé", null);
@@ -29,41 +33,41 @@ export class ProfilesRequests {
     return sendResponse(true, null, profile);
   }
 
-  public async updateProfiles(newUser: DbProfiles) {
-    if (!newUser || !newUser.email || !newUser.password) {
-      return sendResponse(
-        false,
-        "Renseigner l'email et le mot de passe pour inscription",
-        null
-      );
-    }
-    const columns = getKeysCommaSeparated(newUser);
-    const values = getValuesCommaSeparated(newUser);
+  // public async updateProfiles(newUser: DbProfiles) {
+  //   if (!newUser || !newUser.email || !newUser.password) {
+  //     return sendResponse(
+  //       false,
+  //       "Renseigner l'email et le mot de passe pour inscription",
+  //       null
+  //     );
+  //   }
+  //   const columns = getKeysCommaSeparated(newUser);
+  //   const values = getValuesCommaSeparated(newUser);
 
-    // const profile =
-    //   await db`INSERT INTO ${PROFILES}(${columns}) VALUES(${values}) ON CONFLICT(email) DO NOTHING RETURNING *`;
+  //   // const profile =
+  //   //   await db`INSERT INTO ${PROFILES}(${columns}) VALUES(${values}) ON CONFLICT(email) DO NOTHING RETURNING *`;
 
-    // if (!profile) {
-    //   return sendResponse(false, "L'email fourni est dejà utilisé", null);
-    // }
+  //   // if (!profile) {
+  //   //   return sendResponse(false, "L'email fourni est dejà utilisé", null);
+  //   // }
 
-    // return sendResponse(true, null, profile);
-  }
+  //   // return sendResponse(true, null, profile);
+  // }
 
-  public async softDeleteProfiles(id: string) {
-    if (!id) {
-      return sendResponse(false, "Renseigner l'id du compte à supprimer", null);
-    }
+  // public async softDeleteProfiles(id: string) {
+  //   if (!id) {
+  //     return sendResponse(false, "Renseigner l'id du compte à supprimer", null);
+  //   }
 
-    // const profile =
-    //   await db`INSERT INTO ${PROFILES}(${columns}) VALUES(${values}) ON CONFLICT(email) DO NOTHING RETURNING *`;
+  //   // const profile =
+  //   //   await db`INSERT INTO ${PROFILES}(${columns}) VALUES(${values}) ON CONFLICT(email) DO NOTHING RETURNING *`;
 
-    // if (!profile) {
-    //   return sendResponse(false, "L'email fourni est dejà utilisé", null);
-    // }
+  //   // if (!profile) {
+  //   //   return sendResponse(false, "L'email fourni est dejà utilisé", null);
+  //   // }
 
-    // return sendResponse(true, null, profile);
-  }
+  //   // return sendResponse(true, null, profile);
+  // }
 }
 
 export const profilesRequests = new ProfilesRequests();
