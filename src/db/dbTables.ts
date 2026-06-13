94edd9ffb92af954aeba763197e1577707ae41a2
KUROYUKIHIME333 <dhermansum@gmail.com>
2026-06-13 11:48:20 +0100

    all db tables

---
+++ src/db/dbTables.ts
@@ -0,0 +1,8 @@
+export const PROFILES = "profiles";
+export const SUBSCRIPTIONS = "subscriptions";
+export const SUBSCRIPTIONS_PLAN = "subscription_plans";
+export const PROJECTS = "projects";
+export const CALCULATIONS = "calculations";
+export const JOBS_QUEUE = "jobs_queue";
+export const JOURNAL_ACTIONS = "actions_journal";
+export const JOURNAL_DELETIONS = "deleted_journal";
