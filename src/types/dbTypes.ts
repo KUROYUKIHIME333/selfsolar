ff95430f5e0af07e65692213560b416a015f1445
KUROYUKIHIME333 <dhermansum@gmail.com>
2026-06-13 11:48:31 +0100

    types of the db tables

---
+++ src/types/dbTypes.ts
@@ -0,0 +1,35 @@
+export type TypePlanSouscription = "FREE" | "STANDARD" | "PRO";
+
+export interface DbProfiles {
+  id: string;
+  profile_picture: string;
+  username: string;
+  company_name: string;
+  email: string;
+  is_delete: boolean;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export interface DbSubscriptionPlans {
+  plan: TypePlanSouscription;
+  price: number;
+  currency: string;
+  descript?: string;
+  is_delete?: boolean;
+  id?: string;
+  profile_icon?: string;
+  created_at?: Date;
+  updated_at?: Date;
+}
+
+export interface DbSubscriptions {
+  user_id: string;
+  plan_subscriptions: string;
+  is_active?: boolean;
+  id?: string;
+  is_delete?: boolean;
+  expires_at?: Date;
+  created_at?: Date;
+  updated_at?: Date;
+}
