db0d7c9bb66753440512e62cd1641fa9e80ddcbb
KUROYUKIHIME333 <dhermansum@gmail.com>
2026-06-14 08:10:43 +0100

    working

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,16 +1,21 @@
 import postgres from "postgres";
 import { env } from "./env.js";
 
-const useSupabaseDb: boolean = env.DATA_BASE_USED === "postgresql_supabase";
-const useLocalPostgresqlDb: boolean = env.DATA_BASE_USED === "postgresql_local";
+// Constants for data base used
+const useSupabaseDb = env.DATA_BASE_USED === "postgresql_supabase";
+const useLocalPostgresqlDb = env.DATA_BASE_USED === "postgresql_local";
 
+// Database URL for local PostgreSQL database
 const clientUrl: string = useLocalPostgresqlDb
   ? `postgres://${env.POSTGRES_USER}:${env.POSTGRES_PASSWORD}@${env.POSTGRES_HOST}:${env.POSTGRES_PORT}/${env.POSTGRES_DB}`
   : env.SUPABASE_DB_URL;
 
+// Create a connection to the database using PostgreSQL
 const db = postgres(clientUrl, {
   max: 15,
   ssl: useSupabaseDb ? "require" : false,
 });
+
+console.log(db);
 
 export default db;
