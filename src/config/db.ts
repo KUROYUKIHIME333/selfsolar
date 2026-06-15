f8e98c8c71eb4ec9a9d0ab9700bfbdc54055443d
KUROYUKIHIME333 <dhermansum@gmail.com>
2026-06-15 14:45:27 +0100

    working on it

--- src/config/db.ts
+++ src/config/db.ts
@@ -7,6 +7,14 @@ const connectionString = !isSupabase
   ? `postgres://${env.POSTGRES_USER}:${env.POSTGRES_PASSWORD}@${env.POSTGRES_HOST}:${env.POSTGRES_PORT}/${env.POSTGRES_DB}`
   : env.SUPABASE_DB_URL;
 
+//TODO: retirer après debuging
+if (!connectionString) {
+  throw new Error(
+    "La variable d'environnement de connexion à la base de données est manquante."
+  );
+}
+
 export const sql = postgres(connectionString, {
+  connect_timeout: 10,
   ssl: isSupabase ? "require" : false,
 });
