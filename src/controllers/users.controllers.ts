72022defb8b417ca1797a14ffddaad20ca2a8713
KUROYUKIHIME333 <dhermansum@gmail.com>
2026-06-15 17:30:15 +0100

    user controller : method creerNouveauProfile

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -6,28 +6,52 @@ import { sendError, sendSuccess } from "../utils/handlers.utils.js";
 export class UserController {
   public async creerNouveauProfile(
     request: FastifyRequest<{
-      Body: DbProfiles;
+      Body: { newUser: DbProfiles };
     }>,
     reply: FastifyReply
   ) {
     console.log(request.body);
 
     try {
-      const newUser = request.body;
+      const { newUser } = request.body;
 
-      console.log(newUser || "rien à montrer");
+      if (newUser?.password && newUser?.email) {
+        const existingUser = await profilesRequests.findByEmail(newUser.email);
 
-      if (!newUser || !newUser.email || !newUser.password) {
-        return sendError(
-          reply,
-          "Email et mot de passe doivent être renseignés",
-          400
-        );
+        if (existingUser) {
+          return sendError(
+            reply,
+            "Email fourni déjà utilisé pour un compte",
+            400
+          );
+        }
+
+        const user = await profilesRequests.createUser(newUser);
+
+        if (user) {
+          return sendSuccess(
+            reply,
+            {
+              id: user.id,
+              picture: user.profile_picture || null,
+              username: user.username || null,
+              company: user.company_name || null,
+              email: user.email,
+              isActive: user.is_active,
+              creation: user.created_at,
+            },
+            200
+          );
+        }
+
+        return sendError(reply, "Désolé, utilisateur non créé", 500);
       }
 
-      const result = await profilesRequests.createProfiles(newUser);
-
-      return sendSuccess(reply, result, 200);
+      return sendError(
+        reply,
+        "L'email et le mot de passe (hashé) doivent être renseignés",
+        400
+      );
     } catch (error: unknown) {
       return sendError(reply, error, 500);
     }
