245321ac9bc61c15aa243c231950c49b1989f3eb
KUROYUKIHIME333 <dhermansum@gmail.com>
2026-06-15 15:55:50 +0100

    Trying to fix it also, after some galère

--- src/db/requests/usersAndProfiles/requests.ts
+++ src/db/requests/usersAndProfiles/requests.ts
@@ -1,23 +1,18 @@
-import { db } from "../../../config/db.js";
-import { PROFILES, PROFILES_FIELDS_TO_SEND } from "../../dbTables.js";
-import type { DbProfiles } from "../../../types/dbTypes.js";
 import { basicRequests } from "../../basicRequests.js";
-import { getKeysArray } from "../../../utils/toolbox.utils.js";
+import { PROFILES, PROFILES_FIELDS_TO_SEND } from "../../dbTables.js";
 export class ProfilesRequests {
-  public async createProfiles(newUser: DbProfiles) {
-    const sqlRequest = basicRequests.insertIntoTable(
+  public async findByEmail(email: string) {
+    const result = await basicRequests.selectByFieldFromTable(
       PROFILES,
-      newUser,
-      PROFILES_FIELDS_TO_SEND
+      "email",
+      email
     );
-    const columns = getKeysArray(newUser);
-
-    console.log(sqlRequest);
-
-    const response = await db`insert into profiles ${db(newUser, columns)}`;
-
-    return response;
+    return result
   }
+
+  public async countActiveProfiles(){
+    const result = await basicRequests.countByAFieldFromTable(PROFILES, )
+  } 
 }
 
 export const profilesRequests = new ProfilesRequests();
