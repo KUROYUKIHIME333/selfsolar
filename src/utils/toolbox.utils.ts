34d74c15a7a5be138dba3ce629712a054372778d
KUROYUKIHIME333 <dhermansum@gmail.com>
2026-06-15 13:20:47 +0100

    fix

--- src/utils/toolbox.utils.ts
+++ src/utils/toolbox.utils.ts
@@ -7,13 +7,21 @@ export const getValuesInAnArray = (obj: Record<string, any>): string[] => {
 };
 
 export const getKeysCommaSeparated = (obj: Record<string, any>): string => {
-  return Object.keys(obj).join(",");
+  return Object.keys(obj).join(", ");
+};
+
+export const getKeysArray = (obj: Record<string, any>): string[] => {
+  return Object.keys(obj);
 };
 
 export const getValuesCommaSeparated = (obj: Record<string, any>): string => {
   let values: string[] = [];
   Object.values(obj).forEach((element) => {
-    values.push(`'${element}'`);
+    if (typeof element === "boolean" || typeof element === "number") {
+      values.push(`${element}`);
+    } else {
+      values.push(`'${element}'`);
+    }
   });
-  return values.join(",");
+  return values.join(", ");
 };
