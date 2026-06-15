2468fb78f77d35df1d8df9ed86fcdb2ab6fd6f60
KUROYUKIHIME333 <dhermansum@gmail.com>
2026-06-15 15:53:10 +0100

    UPDATE

--- src/types/dbTypes.ts
+++ src/types/dbTypes.ts
@@ -1,38 +1,37 @@
-import { PROFILES } from "../db/dbTables.js";
-
 export type TypePlanSouscription = "FREE" | "STANDARD" | "PRO";
 
 export interface DbProfiles {
+  id: string | undefined;
+  profile_picture: string | undefined;
+  username: string | undefined;
+  company_name: string | undefined;
   email: string;
-  password?: string;
-  id?: string | undefined;
-  profile_picture?: string | undefined;
-  username?: string | undefined;
-  company_name?: string | undefined;
-  is_delete?: boolean | undefined;
-  created_at?: Date | undefined;
-  updated_at?: Date | undefined;
+  password: string;
+  is_active: undefined | boolean;
+  is_delete: boolean | undefined;
+  created_at: Date | undefined;
+  updated_at: Date | undefined;
 }
 
 export interface DbSubscriptionPlans {
+  id: undefined | string;
+  profile_icon: undefined | string;
   plan: TypePlanSouscription;
   price: number;
   currency: string;
-  descript?: undefined | string;
-  is_delete?: undefined | boolean;
-  id?: undefined | string;
-  profile_icon?: undefined | string;
-  created_at?: undefined | Date;
-  updated_at?: undefined | Date;
+  descript: undefined | string;
+  is_delete: undefined | boolean;
+  created_at: undefined | Date;
+  updated_at: undefined | Date;
 }
 
 export interface DbSubscriptions {
+  id: undefined | string;
   user_id: string;
   plan_subscriptions: string;
-  is_active?: undefined | boolean;
-  id?: undefined | string;
-  is_delete?: undefined | boolean;
-  expires_at?: undefined | Date;
-  created_at?: undefined | Date;
-  updated_at?: undefined | Date;
+  is_active: undefined | boolean;
+  is_delete: undefined | boolean;
+  expires_at: undefined | Date;
+  created_at: undefined | Date;
+  updated_at: undefined | Date;
 }
