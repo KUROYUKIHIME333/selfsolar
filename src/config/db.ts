074ef6591d06958da898735836f15e4b0d1f077e
KUROYUKIHIME333 <dhermansum@gmail.com>
2026-06-15 14:28:21 +0100

    Try to fix the db config

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,21 +1,12 @@
 import postgres from "postgres";
 import { env } from "./env.js";
 
-// Constants for data base used
-const useSupabaseDb = env.DATA_BASE_USED === "postgresql_supabase";
-const useLocalPostgresqlDb = env.DATA_BASE_USED === "postgresql_local";
+const isSupabase = env.DATA_BASE_USED === "postgresql_supabase";
 
-// Database URL for local PostgreSQL database
-const clientUrl: string = useLocalPostgresqlDb
+const connectionString = !isSupabase
   ? `postgres://${env.POSTGRES_USER}:${env.POSTGRES_PASSWORD}@${env.POSTGRES_HOST}:${env.POSTGRES_PORT}/${env.POSTGRES_DB}`
   : env.SUPABASE_DB_URL;
 
-// Create a connection to the database using PostgreSQL
-const db = postgres(clientUrl, {
-  max: 15,
-  ssl: useSupabaseDb ? "require" : false,
+export const sql = postgres(connectionString, {
+  ssl: isSupabase ? "require" : false,
 });
-
-console.log(db);
-
-export default db;
