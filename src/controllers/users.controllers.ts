ec8f2038fc3832f7c7515bb62b5d981fe46c4d92
KUROYUKIHIME333 <dhermansum@gmail.com>
2026-06-16 21:39:39 +0100

    update

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -16,12 +16,9 @@ export class UserController {
       const { newUser } = request.body;
 
       if (newUser?.password && newUser?.email) {
-        console.log(newUser.email);
-        console.log(newUser.password);
-
         const existingUser = await profilesRequests.findByEmail(newUser.email);
 
-        if (existingUser) {
+        if (existingUser.length > 0) {
           return sendError(
             reply,
             "Email fourni déjà utilisé pour un compte",
@@ -34,23 +31,33 @@ export class UserController {
         //TODO: debugging to remove
         console.log(user);
 
-        if (user) {
-          return sendSuccess(
+        if (user.length > 0) {
+          console.error("PROBLEME CRITIQUE: INSERTION MULTIPLE DETECTEE");
+
+          return sendError(
             reply,
-            {
-              id: user.id,
-              picture: user.profile_picture || null,
-              username: user.username || null,
-              company: user.company_name || null,
-              email: user.email,
-              isActive: user.is_active,
-              creation: user.created_at,
-            },
-            200
+            "PROBLEME CRITIQUE: INSERTION MULTIPLE DETECTEE",
+            500
           );
         }
 
-        return sendError(reply, "Désolé, utilisateur non créé", 500);
+        if (!user || !user[0]) {
+          return sendError(reply, "Désolé, utilisateur non créé", 500);
+        }
+
+        return sendSuccess(
+          reply,
+          {
+            id: user[0].id,
+            picture: user[0].profile_picture || null,
+            username: user[0].username || null,
+            company: user[0].company_name || null,
+            email: user[0].email,
+            isActive: user[0].is_active,
+            creation: user[0].created_at,
+          },
+          200
+        );
       }
 
       return sendError(
@@ -58,6 +65,37 @@ export class UserController {
         "L'email et le mot de passe (hashé) doivent être renseignés",
         400
       );
+    } catch (error: unknown) {
+      return sendError(reply, error, 500);
+    }
+  }
+
+  public async modifierUtilisateurExistant(
+    request: FastifyRequest<{
+      Body: {
+        id: string;
+        newDatas: DbProfiles;
+      };
+    }>,
+    reply: FastifyReply
+  ) {
+    try {
+      const { id, newDatas } = request.body;
+
+      if (newDatas) {
+        const updateUser = await profilesRequests.updateUser(id, newDatas);
+
+        if (updateUser.length > 0) {
+          return sendError(
+            reply,
+            "Problème critique: modificaton multiple",
+            400
+          );
+        }
+        if (!user || !user[0]) {
+          return sendError(reply, "Désolé, utilisateur non créé", 500);
+        }
+      }
     } catch (error: unknown) {
       return sendError(reply, error, 500);
     }
