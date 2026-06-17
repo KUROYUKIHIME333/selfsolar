85ad316183cceda971a21432c04a887570c20d3f
KUROYUKIHIME333 <dhermansum@gmail.com>
2026-06-17 15:36:25 +0100

    Begin the auth routes

---
+++ src/routes/auth.routes.ts
@@ -0,0 +1,9 @@
+import { FastifyInstance } from "fastify";
+import { installationPhotovoltaiqueRoutes } from "./routesFiles/installationPhotovoltaique.routes.js";
+import { usersRoutes } from "./routesFiles/users.routes.js";
+
+export const authRoutes = async (app: FastifyInstance) => {
+  // Routes PV sous préfixe /api/v1/pv
+  app.register(installationPhotovoltaiqueRoutes, { prefix: "/api/v1/pv" });
+  app.register(usersRoutes, { prefix: "/use" });
+};
